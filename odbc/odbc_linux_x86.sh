c08f809b071a32be4a843460327e33f034949092
jbShi1017 <154418562+jbShi1017@users.noreply.github.com>
2025-01-10 09:24:11 +0800

    update

--- odbc/odbc_linux_x86.sh
+++ odbc/odbc_linux_x86.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -x
+set -xe
 
 ODBC_BRANCH=$1
 SERVER_VERSION=$2
@@ -32,20 +32,24 @@ docker restart odbc_auto_test
 # 启动server
 docker exec odbc_auto_test bash -c "cd /root/odbc_auto_test/workdir/single && sh ./startSingle.sh"
 # 拉取代码
+set +x
 git clone -b $ODBC_BRANCH https://jianbo.shi%40dolphindb.com:%21s1017539527@dolphindb.net/dolphindb/dolphindb-odbc.git $WORK_DIR/workdir/codes
+set -x
 # 获取lib
 for i in libddbodbc.so libDolphinDBAPI.so
 do
     wget "ftp://ftp.dolphindb.cn/origin/$ODBC_BRANCH/Release/dolphindb-odbc/ABI0/1.0.2u/$i" --ftp-user=ftpuser --ftp-password=DolphinDB123 -O $WORK_DIR/workdir/$i
 done
 # 修改配置
-echo -e "[DolphinDB ODBC Driver]\nDescription=DolphinDB ODBC Driver\nDriver=/root/odbc_auto_test/workdir/libddbodbc.so" > /root/unixODBC/etc/odbcinst.ini
-echo -e "[dolphindb]\nDriver=DolphinDB ODBC Driver\nServer=192.168.100.26\nDatabase=dfs://testdb\nPort=35998\nUid=admin\nPwd=123456" > /root/unixODBC/etc/odbc.ini
+echo -e "[DolphinDB ODBC Driver]\nDescription=DolphinDB ODBC Driver\nDriver=/root/odbc_auto_test/workdir/libddbodbc.so" > $WORK_DIR/workdir/odbcinst.ini
+echo -e "[dolphindb]\nDriver=DolphinDB ODBC Driver\nServer=192.168.100.26\nDatabase=dfs://testdb\nPort=35998\nUid=admin\nPwd=123456" > $WORK_DIR/workdir/odbc.ini
+docker exec odbc_auto_test bash -c "cp -f /root/odbc_auto_test/workdir/odbc*.ini /root/unixODBC/etc/"
+cp -f $WORK_DIR/../prepare/odbc/config/config.cpp $WORK_DIR/workdir/codes/test/config.cpp
 # 编译
 mkdir -p $WORK_DIR/workdir/codes/build
 docker exec odbc_auto_test bash -c "cd /root/odbc_auto_test/workdir/codes/build && cmake .. -DRUN_GTEST=ON -DBUILD_ONLY_GTEST=ON && make -j"
 # test
-docker exec odbc_auto_test bash -c "cd /root/odbc_auto_test/workdir/codes/build && LD_LIBRARY_PATH=/root/odbc_auto_test/workdir:/root/unixODBC/lib ./BasicTest --gtest_output=xml:output.xml"
+docker exec odbc_auto_test bash -c "cd /root/odbc_auto_test/workdir/codes/build && LD_LIBRARY_PATH=/root/odbc_auto_test/workdir:/root/unixODBC/lib ./BasicTest --gtest_output=xml:output.xml" || true
 cp -f $WORK_DIR/workdir/codes/build/output.xml $WORK_DIR/workdir/output.xml
 cp -f $WORK_DIR/../prepare/odbc/gtest.xsl $WORK_DIR/workdir/gtest.xsl
 cp -f $WORK_DIR/../prepare/odbc/report.py $WORK_DIR/workdir/report.py
